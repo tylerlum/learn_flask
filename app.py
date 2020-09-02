39a756cca73ce7330e233a9fed24eec9463e5505
Tyler Lum <tylergwlum@gmail.com>
2020-09-02 12:31:28 -0700

    Add update functionality

--- app.py
+++ app.py
@@ -49,6 +49,22 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
+# Update functionality
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task)
+
 # Run the flask app
 if __name__ == "__main__":
     app.run(debug=True)
